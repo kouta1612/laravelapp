97721955c529f00b05475ae31aa95d2047bb1629
kouta <kouta1612world69@gmail.com>
2018-05-11 00:46:25 +0000

    dotinstall#31

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \URL::forceScheme('https');
+        if(\DB::getDriverName() === 'sqlite') {
+            \DB::statement(\DB::raw('PRAGMA foreign_keys=1'));
+        }
     }
 
     /**
